7e8d14bef16412eca3a6fdd9fbd9fdee746a55d5
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-10-07 20:16:44 -0300

    add equal and be

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe 'equality matchers' do
             expect(a).not_to eql(b)
         end
     end
+
+    describe 'equal and be matcher' do
+        let(:c) {[1,2,3]}
+        let(:d) {[1,2,3]}
+        let(:e) { c }
+
+        it 'cares about object identity' do
+            expect(c).to eq(d)
+            expect(c).to eql(d)
+
+            expect(c).to equal(e)
+            expect(c).to be(e)
+
+            expect(c).not_to equal(d)
+            expect(c).not_to equal([1,2,3])
+        end
+    end
 end
