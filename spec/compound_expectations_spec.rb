28f775b00c1b9fe7ac0c721970ba7b7367e7598e
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-10-19 19:53:11 -0300

    add more examples for compound expectations

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 25 do
         #expect(subject).to be > 20
 
         expect(subject).to be_odd.and be > 20
+        expect('firetruck').to start_with('fire').and end_with('truck')
+        expect(20).to be_even.and respond_to(:times)
+        expect([4,8,15,16,23,42]).to include(42).and start_with([4, 8, 15])
     end
 
     it { is_expected.to be_odd.and be > 20 }
