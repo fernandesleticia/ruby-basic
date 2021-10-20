8aa57d5a84d50e79912b3bbadc1a69945e059eef
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-10-20 20:39:49 -0300

    test examples for movie class

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -40,6 +40,8 @@ class Actor
 end
 
 class Movie
+    attr_reader :actor
+
     def initialize(actor)
         @actor = actor
     end
@@ -53,6 +55,17 @@ class Movie
     end
 end
 
-actor = Actor.new('Brad Pitt')
-movie = Movie.new(actor)
-movie.start_shooting
+RSpec.describe Movie do
+    let(:stuntman) { double('Mr. Danger', ready?: true, act: 'Any string at all', fall_off_ladder: 'Sure! Lets do it', light_on_fire: true) }
+    subject { described_class.new(stuntman) }
+
+    describe '#start_shooting method' do
+        it 'expect a actor to do 3 actions' do
+            expect(stuntman).to receive(:ready?) 
+            expect(stuntman).to receive(:act) 
+            expect(stuntman).to receive(:fall_off_ladder) 
+            expect(stuntman).to receive(:light_on_fire) 
+            subject.start_shooting
+        end
+    end
+end
