1066f33d4c2499039c4e44449d386aa2c545a24c
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-10-20 20:27:12 -0300

    setting up movie test

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -15,3 +15,44 @@ RSpec.describe 'a random double' do
         expect(stuntman.light_on_fire).to eq(true)
     end
 end
+
+class Actor
+    def initialize(name)
+        @name = name
+    end
+
+    def ready?
+        sleep(3)
+        true
+    end
+
+    def act
+        'I love you baby'
+    end
+
+    def fall_off_ladder
+        'Call my agent! No way!'
+    end
+
+    def light_on_fire
+        false
+    end
+end
+
+class Movie
+    def initialize(actor)
+        @actor = actor
+    end
+
+    def start_shooting
+        if actor.ready?
+            actor.act
+            actor.fall_off_ladder
+            actor.light_on_fire
+        end
+    end
+end
+
+actor = Actor.new('Brad Pitt')
+movie = Movie.new(actor)
+movie.start_shooting
