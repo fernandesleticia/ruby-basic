c2c3a00ed320e9f09d1fa5ae0cc13facce605c56
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-10-21 21:40:27 -0300

    add examples for receive counts

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -51,6 +51,7 @@ class Movie
             actor.act
             actor.fall_off_ladder
             actor.light_on_fire
+            actor.act
         end
     end
 end
@@ -61,10 +62,14 @@ RSpec.describe Movie do
 
     describe '#start_shooting method' do
         it 'expect a actor to do 3 actions' do
-            expect(stuntman).to receive(:ready?) 
-            expect(stuntman).to receive(:act) 
-            expect(stuntman).to receive(:fall_off_ladder) 
-            expect(stuntman).to receive(:light_on_fire) 
+            #expect(stuntman).to receive(:light_on_fire).once
+            #expect(stuntman).to receive(:light_on_fire).exactly(1).times
+            expect(stuntman).to receive(:light_on_fire).at_most(1).times
+
+            #expect(stuntman).to receive(:act).twice
+            #expect(stuntman).to receive(:act).exactly(2).times
+            expect(stuntman).to receive(:act).at_least(2)
+
             subject.start_shooting
         end
     end
