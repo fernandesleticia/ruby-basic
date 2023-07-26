d7d53d24d4717e511f9f49eb68cbc7ddde87e739
Leticia <leticiafernandes777@gmail.com>
2023-07-26 10:07:39 -0300

    clean code

--- using_bcrypt.rb
+++ using_bcrypt.rb
@@ -1,34 +1,30 @@
 module UsingBcrypt
-    require 'bcrypt'
+  require 'bcrypt'
 
-    def create_hash_digest(password)
-        BCrypt::Password.create(password)
+  def create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+
+  def verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  def create_secure_users(users)
+    users.each do |user|
+      user[:password] = create_hash_digest(user[:password])
     end
 
-    def verify_hash_digest(password)
-        BCrypt::Password.new(password)
+    users
+  end
+
+  def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user|
+      if user[:username] == username && 
+        verify_hash_digest(user[:password]) == password
+        return user
+      end
     end
 
-    def create_secure_users(users)
-        users.each do |user|
-            user[:password] = create_hash_digest(user[:password])
-        end
-
-        users
-    end
-
-    #users_with_digest = create_secure_users(users)
-
-    def authenticate_user(username, password, list_of_users)
-        list_of_users.each do |user|
-            if user[:username] == username && 
-                verify_hash_digest(user[:password]) == password
-                return user
-            end
-        end
-        'Credentails were not correct'
-    end
-
-    #puts authenticate_user('let', '123', users_with_digest)
-
+    'Credentails were not correct'
+  end
 end
